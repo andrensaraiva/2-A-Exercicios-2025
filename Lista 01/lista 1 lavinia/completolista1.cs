f68ef103b987aeffdb597d74a1f0ff40dce57bb7
LaviniaSantos1 <lavisou.santos@icloud.com>
2025-06-05 09:57:17 -0300

    Add files via upload

---
+++ Lista 01/lista 1 lavinia/completolista1.cs
@@ -0,0 +1,53 @@
+﻿
+Console.WriteLine("Meu primeiro programa em PY para Jogos Digitais é REAL!");
+
+Console.WriteLine("Qual sua palavra favorita?");
+String palavra = Console.ReadLine();
+Console.WriteLine("Sua palavra favorita é: " + palavra);
+
+Console.WriteLine("Qual seu nome de herói/heroína");
+string nome = Console.ReadLine();
+Console.WriteLine("Bem vindo(a) à jornada, nobre" + nome);
+
+
+Console.WriteLine("Qual seu animal favorito?");
+
+string animal = Console.ReadLine();
+
+Console.WriteLine("Qual sua cor favorita");
+string cor = Console.ReadLine();
+Console.WriteLine("Seu apelido de aventureiro poderia ser: " + animal + cor);
+
+Console.WriteLine(" Qual o nome da sua guilda? ");
+string equipe = Console.ReadLine();
+Console.WriteLine(" Nome:" + equipe);
+
+Console.WriteLine(" Qual o lema da sua Guilda");
+string lema = Console.ReadLine();
+Console.WriteLine(" Lema:" + lema);
+
+Console.WriteLine("Qual reino mágico começa a aventura?");
+string reino = Console.ReadLine();
+
+Console.WriteLine("Qual artefato lendário está sendo buscado?");
+string artefato = Console.ReadLine();
+
+Console.WriteLine(" No distante reino de " + reino + ", nossa jornada começa em busca do(a) lendário(a) " + artefato);
+
+Console.WriteLine(" Digite um verbo de ação (ex: lutar, conquistar, defender):");
+string verbo = Console.ReadLine();
+Console.WriteLine(" digite um substantivo épico (ex: Honra, Glória, Justiça)");
+string subs = Console.ReadLine();
+Console.WriteLine(" Qual o nome do seu inimigo principal?");
+string inimigo = Console.ReadLine();
+Console.WriteLine("Pela " + subs + "! Vamos " + verbo + " e derrotar " + inimigo + "!");
+
+Console.WriteLine("Personagem 1: Qual é o seu nome, viajante?");
+string guerreiro = Console.ReadLine();
+Console.WriteLine(" Personagem 1: Seja bem vindo(a), " + nome + " ! Eu sou o Guardião Ancião." +
+nome + ": É uma honra conhecê - lo, guardião.O que me aguarda ó grande Guardião ancião.Grandes desafios e recompensas, jovem" + nome);
+
+
+
+
+
