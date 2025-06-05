daa6557dcb0c97f3c73f7aa62f675ac9b8b9f6bd
lelelopes-ei <chlopesleticia@gmail.com>
2025-06-05 08:44:18 -0300

    Add files via upload

---
+++ Lista 01/Lista 01 - LeticiaChaves/exercício04.cs
@@ -0,0 +1,9 @@
+﻿string Arquivo1 = "salvo";
+Console.WriteLine("Qual é o seu animal favorito?");
+Arquivo1 = Console.ReadLine();
+
+string Arquivo2 = "salvo";
+Console.WriteLine("Qual a sua cor favorita?");
+Arquivo2 = Console.ReadLine();
+
+Console.WriteLine("Seu apelido de aventureito poderia ser: " +  Arquivo1 + Arquivo2);
---
+++ Lista 01/Lista 01 - LeticiaChaves/exercício08.cs
@@ -0,0 +1,11 @@
+﻿string Nome = "salvo";
+Console.WriteLine("Personagem 1: Qual é o seu nome, viajante?");
+Nome = Console.ReadLine();
+
+Console.WriteLine("Personagem 1: Seja bem-vindo(a), " + Nome + "! Eu sou o Guardião Ancião.");
+
+
+Console.WriteLine(Nome + ": É uma honra conhecê-lo, Guardião. O que me aguarda?.");
+
+
+Console.WriteLine("Guardião Ancião: Grandes desafios e recompensas, jovem " + Nome + ".");
