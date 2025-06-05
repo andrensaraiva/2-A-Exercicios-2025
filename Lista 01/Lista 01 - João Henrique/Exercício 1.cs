5585667b13832ec5df643c45937079588252f8e2
27Jotta27 <joaohenriquercarmo@gmail.com>
2025-06-05 08:32:56 -0300

    Add files via upload

---
+++ Lista 01/Lista 01 - João Henrique/Exercício 1.cs
@@ -0,0 +1,11 @@
+﻿int vida = 100;
+float altura = 1.8f;
+bool vf = false;
+
+
+Console.WriteLine("Digite um numero");
+vida = int.Parse (Console.ReadLine());
+
+vida = vida / 2;
+
+Console.WriteLine("sua vida é: " +vida);
