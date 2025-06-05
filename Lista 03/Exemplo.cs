2b976c840d7687b9d6f45afc1697aa16bea7a300
Andre Saraiva <andrensaraiva@hotmail.com>
2025-06-05 09:38:10 -0300

    Add files via upload

---
+++ Lista 03/Exemplo.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int numero = 0;
+float virgula = 0.1f;
+string nome = "monalisa";
+bool vf = false;
+
+Console.WriteLine("digite um numero");
+numero = int.Parse(Console.ReadLine());
+if (numero >= 5)
+{
+    Console.WriteLine("numero é maior que 5");
+}
+else if (numero < 0)
+{
+    Console.WriteLine("numero é negativo");
+}
+else 
+{
+    Console.WriteLine("numero esta entre 0 e 4");
+}
