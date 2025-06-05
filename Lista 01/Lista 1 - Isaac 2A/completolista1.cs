75b0fa1db0c882c906916756501fe588d8e39fb8
isaaaaaac11 <isaacwrkspace11@gmail.com>
2025-06-05 11:45:34 -0300

    Add files via upload

---
+++ Lista 01/Lista 1 - Isaac 2A/completolista1.cs
@@ -0,0 +1,99 @@
+﻿Console.WriteLine("Meu primeiro programa em PY para Jogos Digitais é REAL!");
+
+Console.WriteLine("Qual sua palavra favorita?");
+
+string palavra = Console.ReadLine();
+
+Console.WriteLine("Sua palavra favorita é: " + palavra);
+
+Console.WriteLine("Qual seu nome de herói/heroína?");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine("Bem-vindo(a) à jornada, nobre: " + nome);
+
+
+Console.WriteLine("Qual seu animal favorito?");
+
+string animal = Console.ReadLine();
+
+Console.WriteLine("Qual sua cor favorita?");
+
+string cor = Console.ReadLine();
+
+Console.WriteLine("Seu apelido de aventureiro poderia ser: " + animal + cor);
+
+Console.WriteLine("Qual o nome da sua Guilda?");
+
+string equipe = Console.ReadLine();
+
+Console.WriteLine("Qual o lema da sua Guilda?");
+
+string lema = Console.ReadLine();
+
+Console.WriteLine("Nome:" + equipe);
+
+Console.WriteLine("Lema:" + lema);
+
+Console.WriteLine("qual reino mágico começa a aventura?");
+
+string reino = Console.ReadLine();
+
+Console.WriteLine("qual artefato lendário está sendo buscado?");
+
+string artefato = Console.ReadLine();
+
+Console.WriteLine("No distante Reino de " + reino + ", nossa jornada começa em busca do(a) lendário(a) " + artefato);
+
+Console.WriteLine("digite um verbo de ação (ex: Lutar, Conquistar, Defender):");
+
+string verbo = Console.ReadLine();
+
+Console.WriteLine("digite um substantivo épico (ex: Honra, Glória, Justiça):");
+
+string subs = Console.ReadLine();
+
+Console.WriteLine("Qual o nome do seu inimigo principal?");
+
+string inimigo = Console.ReadLine();
+
+Console.WriteLine("Pela " + subs + "! Vamos " + verbo + "  e derrotar " + inimigo + "! ");
+
+Console.WriteLine("Personagem 1: Qual seu nome ,viajante? ");
+
+string viajante = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a), " + viajante + "! Eu sou o Guardião Ancião.");
+
+Console.WriteLine(viajante + ": É uma honra conhecê-lo, Guardião. O que me aguarda?");
+
+Console.WriteLine("Grandes desafios e recompensas, jovem " + viajante);
+
+Console.WriteLine("Me dê três ingredientes.");
+
+Console.WriteLine("Primeiro ingreditente:");
+
+string ingrediente1 = Console.ReadLine();
+
+Console.WriteLine("Segundo ingrediente:");
+
+string ingrediente2 = Console.ReadLine();
+
+Console.WriteLine("Terceiro ingrediente:");
+
+string ingrediente3 = Console.ReadLine();
+
+Console.WriteLine("Dê um nome para sua poção:");
+
+string nomepocao = Console.ReadLine();
+
+Console.WriteLine("Receita da poção: " + nomepocao +
+
+    "1. Adicione " + ingrediente1 +
+
+    "2. Misture com " + ingrediente2 +
+
+    "3. Finalize com uma pitada de " + ingrediente3 +
+
+    "Sua poção " + nomepocao + "está pronta!");
+
