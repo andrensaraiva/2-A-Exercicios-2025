3d089d9b1159ca6fd4922975ee176b3b32f101f3
monalisa2277 <monalisasimoes27@gmail.com>
2025-06-05 08:30:35 -0300

    Add files via upload

---
+++ Lista 01/Lista 1 - Monalisa/exercicio 1.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+int vida = 100;
+float altura = 1.8f;
+float vidadecimal = 100f;
+double mesmaCoisa = 1.5;
+bool vf = true;
+string palabra = "fiuohsadiahnjn";
+char umaletra = '9';
+
+Console.WriteLine("Digite um numero");
+vida = int.Parse(Console.ReadLine());
+
+vida = vida * 2;
+
+Console.WriteLine("Sua vida é:" + vida);
