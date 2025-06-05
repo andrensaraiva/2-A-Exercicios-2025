c95ca3dcd92834d95b7c1f1d7c4df87bd41c95ee
Arthur Araújo de Morais <aaraujodemorais@gmail.com>
2025-06-05 09:58:36 -0300

    Add files via upload

---
+++ Lista 1 Arthur Araújo/Completo Lista 1.cs
@@ -0,0 +1,119 @@
+﻿Console.WriteLine("Meu primeiro programa em PY para Jogos Digitais é REAL!");
+
+Console.WriteLine("Qual sua palavra favorita?");
+
+string palavrafavorita = Console.ReadLine();
+
+Console.WriteLine("Você digitou: " + palavrafavorita);
+
+Console.WriteLine("Qual será seu nome de Héroi/Heroína?");
+
+string nome = Console.ReadLine();
+
+Console.WriteLine("Bem vindo(a) á Jornada, nobre " + nome);
+
+Console.WriteLine("Qual seu animal favorito?");
+
+string animal = Console.ReadLine();
+
+Console.WriteLine("Qual sua cor favorita?");
+
+string cor = Console.ReadLine();
+
+Console.WriteLine("Seu apelido de aventureiro poderia ser: " + " O(a) " + animal +  cor);
+
+Console.WriteLine("Qual será o nome da guilda?");
+
+string nomeguilda = Console.ReadLine();
+
+Console.WriteLine("Qual será o lema da guilda?");
+
+string lemaguilda = Console.ReadLine();
+
+Console.WriteLine("Nome: " + nomeguilda);
+Console.WriteLine("Lema: " + lemaguilda);
+
+Console.WriteLine("Qual reino mágico começa a aventura?");
+
+string reino = Console.ReadLine();
+
+Console.WriteLine("Qual artefato lendário está sendo buscado?");
+
+string artefato = Console.ReadLine();
+
+Console.WriteLine("No distante Reino de " + reino + ", nossa jornada começa em busca do(a) lendário(a) " + artefato);
+
+Console.WriteLine("Me fale um verbo de ação: ");
+
+string verbo = Console.ReadLine();
+
+Console.WriteLine("Agora um substantivo épico: ");
+
+string substantivo = Console.ReadLine();
+
+Console.WriteLine("E por último, um nome de um inimigo: ");
+
+string nomeinimigo = Console.ReadLine();
+
+Console.WriteLine("Pela " + substantivo  + "! Vamos " + verbo + " e derrotar " + nomeinimigo);
+
+Console.WriteLine("Personagem 1: Seja bem-vindo(a), " + nome + " ! Eu sou o Guardião Ancião.");
+
+Console.WriteLine(nome + ": É uma honra conhecê-lo, Guardião. O que me aguarda?");
+
+Console.WriteLine("Guardião Ancião: Grandes desafios e recompensas, jovem " + nome);
+
+Console.WriteLine("Me dê três ingredientes.");
+
+Console.WriteLine("Primeiro ingrediente: ");
+
+string ingreditente1 = Console.ReadLine();
+
+
+Console.WriteLine("Segundo ingrediente: ");
+
+string ingreditente2 = Console.ReadLine();
+
+Console.WriteLine("Terceiro ingrediente: ");
+
+string ingreditente3 = Console.ReadLine();
+
+Console.WriteLine("Dê um nome a essa poção: ");
+
+string pocaonome = Console.ReadLine();
+
+Console.WriteLine("Receita da Poção: " + pocaonome);
+Console.WriteLine("1. Adicione " + ingreditente1);
+Console.WriteLine("2. Misture com " + ingreditente2);
+Console.WriteLine("3. Finalize com uma pitada de " + ingreditente3);
+Console.WriteLine("Sua poção " + pocaonome + " está pronta! ");
+
+Console.WriteLine("Um adjetivo: ");
+
+string adjetivo = Console.ReadLine();
+
+Console.WriteLine("Um personagem de jogo que admira: ");
+
+string personagem = Console.ReadLine();
+
+Console.WriteLine("Um verbo no infinitivo: ");
+
+string verboinfinitivo = Console.ReadLine();
+
+Console.WriteLine("Um lugar perigoso: ");
+
+string lugar = Console.ReadLine();
+
+Console.WriteLine("Um objeto mágico: ");
+
+string objeto = Console.ReadLine();
+
+Console.WriteLine("Um sentimento: ");
+
+string sentimento = Console.ReadLine();
+
+Console.WriteLine(personagem + ", o(a) " + adjetivo + ", decidiu " + verboinfinitivo + " até a " + lugar + ". Com o(a) " + objeto + " em mãos e cheio(a) de " + sentimento + 
+    ", nada poderia detê-lo(a)! ");
+
+
+
