728d817a0ebb66e5755cb7dc9c0169a1effd0169
Yas-JD <ajmyasmim@gmail.com>
2025-06-05 08:42:40 -0300

    Add files via upload

---
+++ Lista 01/Lista01 - Yasmim/Exercicio2.cs
@@ -0,0 +1,57 @@
+﻿// Criador de Apelidos Divertidos
+
+String animalFavorito = "Coelho";
+animalFavorito = "Cachorro";
+Console.WriteLine("Escreva qual seu animal favorito:");
+animalFavorito = Console.ReadLine();
+
+Console.WriteLine("Escreva qual seu animal favorito:" + animalFavorito);
+
+String corFavorita = "Lilás";
+corFavorita = "Azul";
+Console.WriteLine("Escreva qual sua cor favorita:");
+corFavorita = Console.ReadLine();
+
+Console.WriteLine("Escreva qual sua cor favorita:" + corFavorita);
+
+Console.WriteLine("Seu apelido de aventureiro poderia ser:O(A)" + animalFavorito + corFavorita!);
+
+// Detalhes da Missão
+
+String nomedaEquipe = "Força Além das Linhas";
+nomedaEquipe = "Estrelas Além do Tempo";
+Console.WriteLine("Escreva o nome da Guilda");
+nomedaEquipe = Console.ReadLine();
+
+Console.WriteLine("Escreva o nome da Guilda" + nomedaEquipe);
+
+String lema = "A união faz a força.";
+lema = "A união faz a força.";
+Console.WriteLine("Escreva o lema da equipe");
+lema = Console.ReadLine();
+
+Console.WriteLine("Escreva o lema da equipe" + lema);
+
+
+// Pequena História Interativa
+
+String nomedoReino = "País Das Maravilhas";
+nomedoReino = "O mágico de Oz";
+Console.WriteLine("Escreva em qual reino mágico começa a aventura");
+nomedoReino = Console.ReadLine();
+
+Console.WriteLine("Escreva em qual reino mágico começa a aventura" + nomedoReino);
+
+String nomedoArtefato = "País Das Maravilhas";
+nomedoArtefato = "O mágico de Oz";
+Console.WriteLine("Escreva em qual reino mágico começa a aventura");
+nomedoArtefato = Console.ReadLine();
+
+Console.WriteLine("Escreva em qual reino mágico começa a aventura" + nomedoArtefato);
+
+
+Console.WriteLine("No distante Reino de" + nomedoReino, "nossa jornada começa em busca do(a) lendário(a)" + nomedoArtefato);
+
+
+
+
---
+++ Lista 01/Lista01 - Yasmim/Exercicio3.cs
@@ -0,0 +1,66 @@
+﻿// Gerador de Grito de Guerra
+
+String verbo_de_ação = "Conquistar";
+verbo_de_ação = "Vencer";
+Console.WriteLine("Escreva um verbo de ação");
+verbo_de_ação = Console.ReadLine();
+
+Console.WriteLine("Escreva um verbo de ação:" + verbo_de_ação);
+
+String substantivoEpico = "Honra";
+substantivoEpico = "Glória";
+Console.WriteLine("Escreva um substantivo épico:");
+substantivoEpico = Console.ReadLine();
+
+Console.WriteLine("Escreva um substantivo épico" + substantivoEpico);
+
+String nomedoInimigo = "Capiroto";
+nomedoInimigo = "Espírito Maléfico";
+Console.WriteLine("Escreva qual o nome do inimigo principal:");
+nomedoInimigo = Console.ReadLine();
+
+Console.WriteLine("Escreva qual o nome do inimigo principal." + nomedoInimigo);
+
+Console.WriteLine("Pela" + substantivoEpico + "Vamos" + verbo_de_ação + "e derrotar" + nomedoInimigo);
+
+// Diálogo entre Personagens
+
+String seuNome = "Analia";
+seuNome = "Yasmim";
+Console.WriteLine("Escreva o seu nome:");
+seuNome = Console.ReadLine();
+
+Console.WriteLine("Seja bem-vindo(a)," + seuNome +"! Eu sou o Guardião Ancião." + seuNome +":É uma honra conhecê-lo, Guardião. O que me aguarda? " + "Guardião Ancião: Grandes desafios e recompensas, jovem" + seuNome);
+
+// Construtor de Poção Maluca
+
+String nomedaPoção = "Receita da Poção";
+nomedaPoção = "Poção Maluca";
+Console.WriteLine("Escreva o nome da Poção");
+nomedaPoção = Console.ReadLine();
+
+Console.WriteLine("Receita da Poção" + nomedaPoção);
+
+
+String nomedoIngrediente1 = "Asa de Morcego";
+nomedoIngrediente1 = "Asa de Morcego";
+Console.WriteLine("Adicione" + nomedoIngrediente1);
+nomedoIngrediente1 = Console.ReadLine();
+
+Console.WriteLine("Adicione" + nomedoIngrediente1);
+
+String nomedoIngrediente2 = "Pó de Fada";
+nomedoIngrediente2 = "Pó de Fada";
+Console.WriteLine("Misture com" + nomedoIngrediente2);
+nomedoIngrediente2 = Console.ReadLine();
+
+Console.WriteLine("Misture com" + nomedoIngrediente2);
+
+String nomedoIngrediente3 = "Lágrima de Unicórnio";
+nomedoIngrediente3 = "Lágrima de Unicórnio";
+Console.WriteLine("Finalize com uma pitada de" + nomedoIngrediente3);
+nomedoIngrediente3 = Console.ReadLine();
+
+Console.WriteLine("Finalize com uma pitada de" + nomedoIngrediente3);
+
+Console.WriteLine("Sua poção" + nomedaPoção + "está pronta"!);
