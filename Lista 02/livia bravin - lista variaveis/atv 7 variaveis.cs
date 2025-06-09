c88a172b089d3156439bbcb6e4fc61ce78cd3d61
livia bravin <livvbravinn@gmail.com>
2025-06-09 08:00:02 -0300

    Add files via upload

---
+++ Lista 02/livia bravin - lista variaveis/atv 7 variaveis.cs
@@ -0,0 +1,8 @@
+﻿int n10 = 10;
+int n11 = 10;
+
+Console.WriteLine("Digite uma altura");
+n10 = int.Parse(Console.ReadLine());
+Console.WriteLine("Digite uma largura");
+n11 = int.Parse(Console.ReadLine());
+Console.WriteLine("Sua área é " + (n10 * n11));
---
+++ Lista 02/livia bravin - lista variaveis/atv variaveis 3.cs
@@ -0,0 +1,5 @@
+﻿int n4 = 17;
+
+Console.WriteLine("Digite sua idade");
+n4 = int.Parse(Console.ReadLine());
+Console.WriteLine("A sua idade em meses é: " + (n4 * 12));
