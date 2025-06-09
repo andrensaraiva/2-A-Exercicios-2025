24127f08aab9e32660a4d099a9afac40a349593c
Isadoravix <rezendeinfo@gmail.com>
2025-06-09 08:06:44 -0300

    Adicionei minha lista completa

---
+++ Lista 01/lista 01 - isadora/Exercico2.cs
@@ -0,0 +1,2 @@
+﻿Console.WriteLine("Você digitou:");
+    Console.ReadLine();
---
+++ Lista 01/lista 01 - isadora/Exercico3.cs
@@ -0,0 +1,7 @@
+﻿string nome = "charlie";
+
+Console.WriteLine("Qual é o seu nome?");
+
+nome = Console.ReadLine();
+
+Console.WriteLine("Bem vindo(a) à jornada, nobre " + nome);
