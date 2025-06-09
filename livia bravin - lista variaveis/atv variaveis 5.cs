cc984eae49a36b9e2660f5cc1e20680861ff59f6
livia bravin <livvbravinn@gmail.com>
2025-06-09 07:58:29 -0300

    Add files via upload

---
+++ livia bravin - lista variaveis/atv variaveis 5.cs
@@ -0,0 +1,8 @@
+﻿string livialinda;
+int n6 = 10;
+
+Console.WriteLine("Qual seu nome?");
+miguel = (Console.ReadLine());
+Console.WriteLine("Digite seu nível");
+n6 = int.Parse(Console.ReadLine());
+Console.WriteLine("Bem vindo(a) " + livialinda + " seu nível é " + n6);
