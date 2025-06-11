29dc63d3366a75e390be90f9534bcf967727dec7
Andre Saraiva <andrensaraiva@hotmail.com>
2025-06-11 16:04:21 -0300

    Add files via upload

---
+++ Lista 01/lista 01 - isadora/Atividade 3-2.cs
@@ -0,0 +1,7 @@
+﻿string lugar = "hogwarts";
+
+Console.WriteLine("O nome do lugar é?");
+
+lugar = Console.ReadLine();
+
+Console.WriteLine("Bem vindo(a) a " + lugar);
---
+++ Lista 01/lista 01 - isadora/nivel medio - AT 3.cs
@@ -0,0 +1,14 @@
+﻿string reino = "hogwarts";
+
+Console.WriteLine("Qual é o reino mágico que começa a aventura?");
+
+reino = Console.ReadLine();
+
+string artefato = "ouro";
+
+Console.WriteLine("Qual é o artefato lendário que está sendo buscado?");
+
+artefato = Console.ReadLine();
+
+
+Console.WriteLine("No distante Reino de " + reino + ", nossa jornada começa em busca do(a) lendário(a) " + artefato);
