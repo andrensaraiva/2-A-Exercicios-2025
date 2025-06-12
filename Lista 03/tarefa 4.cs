b14322436a7d78c3ffaf81fbbed846bc4e2a6326
rafaelpp12 <rafaelpancieripenitenti@gmail.com>
2025-06-12 11:26:01 -0300

    Add files via upload

---
+++ Lista 03/tarefa 4.cs
@@ -0,0 +1,12 @@
+﻿
+int verif = 0;
+Console.WriteLine("Qual o seu nível?");
+verif = int.Parse(Console.ReadLine());
+if (verif >= 5 )
+{
+    Console.WriteLine("você é forte o suficiente, pode entrar para a guilda");
+}
+else
+{
+    Console.WriteLine("HAHA você ainda é fraco");
+}
---
+++ Lista 03/tarefa 6.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics.Arm;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Channels;
+
+int moedas = 0;
+string guilda = "";
+Console.WriteLine("o ferreiro está em promoção para membros da guilda!" + "quantas moedas você tem?");
+moedas = int.Parse(Console.ReadLine());
+Console.WriteLine("hmm... mas você é membro da guilda?");
+guilda = Console.ReadLine();
+
+
+if (moedas >= 50 && guilda == "sim")
+{
+    Console.WriteLine("bom, eu acho que consigo alguma coisa por esse preço para um nobre cavaleiro da guilda");
+}
+else if (moedas < 50 && guilda == "sim")
+{
+    Console.WriteLine("é uma quantidade muito baixa mas vale a pena para um cavaleiro da guilda");
+}
+else if (moedas < 50 && guilda == "não")
+{
+    Console.WriteLine("há, por esse valor não faço nada para alguem que não seja da guilda");
+}
+else if (moedas >= 50 && guilda == "não")
+{
+    Console.WriteLine("é o preço normal de um equipamento, já que você não faz parte da guilda");
+}
+else
+{
+    Console.WriteLine("não entendi, fale direito, rapaz.");
+}
