76d214057cf98993381e14e2147e4703e7f127e3
27Jotta27 <joaohenriquercarmo@gmail.com>
2025-06-12 09:42:40 -0300

    Add files via upload

---
+++ Lista 03/Lista 03 - João Henrique/exercicio 3.cs
@@ -0,0 +1,13 @@
+﻿string b = "b";
+
+Console.WriteLine("A ponte parece instável. Você quer atravessar? (sim/nao)");
+b = Console.ReadLine();
+if (b == "sim")
+{
+    Console.WriteLine("Você atravessa com cuidado e chega em segurança.");
+}
+
+else
+{
+    Console.WriteLine("Você decide procurar outro caminho. Melhor prevenir!");
+}
---
+++ Lista 03/Lista 03 - João Henrique/exercicio 6.cs
@@ -0,0 +1,32 @@
+﻿using System.Runtime.Intrinsics.Arm;
+
+int a;
+string b;
+
+Console.WriteLine("Quantas moedas de ouro você tem?");
+a = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Você é membro da Guilda? (sim/nao)");
+b = (Console.ReadLine());
+
+
+if (a <= 5 && b == "sim")
+{
+    Console.WriteLine("Quer fazer missoes para conseguir moedas?");
+}
+
+
+else if (a > 5 && b == "sim")
+{
+    Console.WriteLine("Otimo! entao vamos para a loja da guilda.");
+}
+
+else if (a > 5 && b == "nao")
+{
+    Console.WriteLine("Entao entre na guilda para acessar a nossa loja!");
+}
+
+else
+{
+    Console.WriteLine("nao posso te ajudar, mal ai");
+}
