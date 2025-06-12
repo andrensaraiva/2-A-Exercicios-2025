ddd7cae68e8784b93f9236bf128347e9a90e72ec
3nzoabr456-crypto <wonderstudent987@gmail.com>
2025-06-12 12:05:49 -0300

    Add files via upload

---
+++ Lista 3 - Enzo Alves/Atividade 1.cs
@@ -0,0 +1,15 @@
+﻿
+string a, b;
+
+Console.WriteLine("Qual é seu nome");
+a = Console.ReadLine();
+Console.WriteLine(a + " Você encotra um poção deseja beber(Sim/Não)");
+b = Console.ReadLine();
+if (b == "Sim")
+{
+    Console.WriteLine("Você bebe a poção! Um calor percorre seu corpo. (HP +10)");
+}
+else
+{
+    Console.WriteLine("Você guarda a poção para depois. Quem sabe o que ela faz?");
+}
---
+++ Lista 3 - Enzo Alves/Atividade 5.cs
@@ -0,0 +1,19 @@
+﻿int a; 
+
+Console.WriteLine("Você encontra uma porta com três botões");
+Console.WriteLine("Qual botão você aperta? (1 = Vermelho, 2 = Azul, 3 = Verde)");
+a = int.Parse(Console.ReadLine());
+if (a == 1)
+{
+    Console.WriteLine("Nada acontece com o botão Vermelho");
+}
+else if (a == 2)
+{
+    Console.WriteLine("A porta se abre! O botão Azul estava certo.");
+}
+else if (a == 3)
+    Console.WriteLine("O botão Verde não funcionou.");
+else
+{
+    Console.WriteLine("Escolha inválida");
+}
