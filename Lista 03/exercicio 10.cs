01fcb3b016c73331f0b1573d620ce0d3ac714001
27Jotta27 <joaohenriquercarmo@gmail.com>
2025-06-12 09:36:15 -0300

    Add files via upload

---
+++ Lista 03/exercicio 10.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.Design;
+
+string a;
+string b;
+string c;
+
+Console.WriteLine("Um grifo está preso em uma armadilha mágica.");
+
+Console.WriteLine("Sua Perícia com Armadilhas é maior que 7 ? (sim / nao)");
+a = (Console.ReadLine());
+
+Console.WriteLine("Você conhece o Feitiço de Dissipação Menor? (sim/nao)");
+b = (Console.ReadLine());
+
+Console.WriteLine("Você possui um Cristal de Amplificação? (sim/nao)");
+c = (Console.ReadLine());
+
+if (a == "sim" || b == "sim" && c == "sim")
+{
+    Console.WriteLine("Voce conseguiu libertar o grifo");
+        
+}
+else
+{
+    Console.WriteLine("O grifo agora está preso por sua culpa");
+}
---
+++ Lista 03/exercicio 4.cs
@@ -0,0 +1,14 @@
+﻿
+int a;
+
+Console.WriteLine("Qual é o seu nível atual?");
+a = int.Parse(Console.ReadLine());
+if (a >= 5)
+{
+    Console.WriteLine("Você é forte o bastante! A caverna se abre.");
+}
+
+else
+{
+    Console.WriteLine("Volte quando estiver mais forte, Elara.");
+}
