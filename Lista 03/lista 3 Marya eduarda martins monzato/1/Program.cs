e2f709c70479d5067c28bbacd3bd3deffe195141
mary-JD <maryduda3411@gmail.com>
2025-06-26 11:53:17 -0300

    Add files via upload

---
+++ Lista 03/lista 3 Marya eduarda martins monzato/1/Program.cs
@@ -0,0 +1,13 @@
+﻿string Resposta = "Sim/Não";
+
+Console.WriteLine("Elara, você quer beber a poção?");
+Resposta = (Console.ReadLine());
+if (Resposta == "Sim")
+{
+    Console.WriteLine("Elara bebeu a poção");
+}
+else 
+{
+    Console.WriteLine("Elara, não bebeu a poção");
+}
+
