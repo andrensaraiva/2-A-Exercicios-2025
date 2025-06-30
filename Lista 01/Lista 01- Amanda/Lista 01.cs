dd17f0cd80deaf4edf7f9ecfb007dc265b663db2
Amanda <amandarodrigueszeferino@gmail.com>
2025-06-30 08:33:37 -0300

    Add files via upload

---
+++ Lista 01/Lista 01- Amanda/Lista 01.cs
@@ -0,0 +1,84 @@
+﻿// Meu primeiro programa em PY para Jogos Digitais é REAL!
+
+// Todas as atividades 
+
+// Atividade 01 
+
+
+    
+        string palavraFavorita = "azul";
+        palavraFavorita = "livro em promoção";
+        Console.WriteLine("diga qual é sua palavra favorita");
+        palavraFavorita = Console.ReadLine();
+        Console.WriteLine("você^digitou: " + palavraFavorita);
+
+
+        // Atividade 02 
+        string nome_do_herói_ou_heroína = "Lilabeth";
+        nome_do_herói_ou_heroína = "Lilobeth";
+        Console.WriteLine("qual é o nome do herói ou heroína");
+        nome_do_herói_ou_heroína = Console.ReadLine();
+        Console.WriteLine("Bem-vindo (a) á jornada, nobre" + nome_do_herói_ou_heroína!);
+
+        // Atividade 03 
+        string nome_guilda;
+        nome_guilda = "Lenux";
+        Console.WriteLine("nome da guilda:");
+        nome_guilda = Console.ReadLine();
+        Console.WriteLine("Nome:" + nome_guilda);
+
+        // Atividade 04
+        string lema_da_guilda;
+        lema_da_guilda = "morte aos traidores";
+        Console.WriteLine("lema");
+        lema_da_guilda = Console.ReadLine();
+        Console.WriteLine("Lema:" + lema_da_guilda);
+
+        // Atividade 05 
+
+        Console.WriteLine("Qual é o seu animal favorito?");
+        string animal = Console.ReadLine();
+        Console.WriteLine("Qual é a sua cor favorita?");
+        string cor = Console.ReadLine();
+        Console.WriteLine("Seu apelido de aventureiro poderia ser: O(A)" + animal + cor);
+
+        // Atividade 06 
+
+
+        Console.WriteLine("verbo de ação");
+        string verbo_de_ação = Console.ReadLine();
+
+        Console.WriteLine("substantivo Épico");
+        string substantivo_épico = Console.ReadLine();
+         Console.WriteLine("Vilão Principal");
+         string vilão_principal = Console.ReadLine();
+         Console.WriteLine("Pela" + substantivo_épico, "Vamos " + verbo_de_ação, + "derrotar" + ,vilão_principal);
+
+string nome_da_porção = "sukita";
+string nome_da_porção = Console.ReadLine();
+string ingrediente_1 = "asa de mosca";
+string ingrediente_1 = Console.ReadLine();
+string inrediente_2 = " jujuba";
+String ingrediente_2 = Console.ReadLine();
+string ingrediente_3 = "pó de fada";
+String ingrediente_3 = Console.ReadLine();
+Console.WriteLine("diga três ingredientes:");
+Console.WriteLine("Receita da Poção:" + nome_da_porção-- - 1. ,"Adicione" + ingrediente_1 ,2. "Misture com" + ingrediente_2 ,3. "Finalize com uma pitada de" + ingrediente_3 "Sua poção" + nome_da_porção "está pronta!");
+
+string personagem = "diva";
+string personagem = Console.ReadLine();
+string adjetivo = "medrosa"
+String adjetivo = Console.ReadLine();
+string personagem_que_admira = "liza";
+String personagem_que_admira = Console.ReadLine();
+string verbo_infinitivo = "correr";
+String verbo_infinitivo = Console.ReadLine();
+string lugar_perigoso = "rua escura";
+String lugar_perigoso = Console.ReadLine();
+string objeto_mágico = "crina de unicórnio";
+String objeto_mágico = Console.ReadLine();
+string sentimento = "amor";
+String sentimento = Console.ReadLine();
+
+Console.WriteLine(+personagem, "o(a)" + adjetivo, "decidiu" + verbo_infinitivo "até a" + lugar_perigoso. "Com o(a)" + objeto_mágico "em mãos e cheio(a) de" + sentimento, " nada poderia detê-lo(a)!");
+
