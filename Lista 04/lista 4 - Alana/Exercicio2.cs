1def23ca79f412e6fc78cb99d37bac87ab963736
martinelli025 <alanamgpimentel@gmail.com>
2025-06-30 09:16:27 -0300

    Add files via upload

---
+++ Lista 04/lista 4 - Alana/Exercicio2.cs
@@ -0,0 +1,33 @@
+﻿
+int acertos = 0;
+string resposta1;
+string resposta2;
+string resposta3;
+
+Console.WriteLine("O que é, o que é? Feito para andar e não anda.");
+resposta1 = Console.ReadLine();
+if 
+    (resposta1 == "rua")
+    {acertos++;}
+
+    Console.WriteLine("O que é, o que é? Dá muitas voltas e não sai do lugar.");
+resposta2 = Console.ReadLine();
+if
+    (resposta2 == "relógio")
+    { acertos++; }
+
+Console.WriteLine("O que é, o que é? Tem cabeça e tem dente, não é bicho e nem é gente.");
+resposta3 = Console.ReadLine();
+if
+    (resposta3 == "alho")
+    { acertos++; }
+
+
+if (acertos == 3)
+{Console.WriteLine("Parabéns, você acertou 3 enigmas e a armadilha foi completamente desativada"); }
+else if (acertos == 2)
+{Console.WriteLine("Foi quase, você acertou 2 enigmas e a armadilha foi parcialmente desativada"); }
+else if (acertos == 1)
+{Console.WriteLine("Não foi dessa vez, você acertou 1 enigma e a armadilha não foi desativada"); }
+else if (acertos == 0)
+{ Console.WriteLine("Não foi dessa vez, você acertou 0 enigmas e a armadilha não foi desativada"); }
---
+++ Lista 04/lista 4 - Alana/Exercicio7.cs
@@ -0,0 +1,17 @@
+﻿string a;
+
+Console.WriteLine("Diálogo com NPC Reativo à Fama");
+Console.WriteLine("Fama do jogador (ex: herói, vilão, neutro)");
+a = Console.ReadLine();
+if (a == "herói")
+{
+    Console.WriteLine("NPC: O grande herói que bom ver você");
+}
+else if (a == "vilão")
+{
+    Console.WriteLine("NPC: Eu não tenho nada aqui para você");
+}
+else if (a == "neutro") ;
+{
+    Console.WriteLine("NPC: Bom dia como vai o dia ");
+}
