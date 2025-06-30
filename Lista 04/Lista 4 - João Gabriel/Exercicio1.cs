fd58a142aeab97dc203c936ed97d3bd6952a4c50
Gabzz <119262507+RDGabzzZ@users.noreply.github.com>
2025-06-30 09:20:32 -0300

    Add files via upload

---
+++ Lista 04/Lista 4 - João Gabriel/Exercicio1.cs
@@ -0,0 +1,19 @@
+﻿using System.Drawing;
+
+string grifo = "sim/nao";
+string escama = "sim/nao";
+string flor = "sim/nao";
+int moeda = 500;
+
+Console.WriteLine("Possui Pena de Grifo?");
+grifo = Console.ReadLine();
+Console.WriteLine("Possui Escama de Basilisco?");
+escama = Console.ReadLine();
+Console.WriteLine("Possui Flor da Lua Sangrenta?");
+flor = Console.ReadLine();
+Console.WriteLine("Quantas moedas de ouro possui?");
+moeda = int.Parse(Console.ReadLine());
+
+if (moeda >= 500 && grifo == "sim" && escama == "sim" && flor == "sim")
+{ Console.WriteLine("Parabéns, você conseguiu fazer a poção."); }
+else { Console.WriteLine("Você não consegue fazer a poção."); }
