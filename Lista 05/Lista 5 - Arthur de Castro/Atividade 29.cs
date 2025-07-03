756aa89cde7aaf3c48b83db03f66421818fb13aa
Arthur Faria <arthurcastrofaria1610@gmail.com>
2025-07-03 11:09:20 -0300

    Update Atividade 29.cs

--- Lista 05/Lista 5 - Arthur de Castro/Atividade 29.cs
+++ Lista 05/Lista 5 - Arthur de Castro/Atividade 29.cs
@@ -1,6 +1,6 @@
-﻿
+
 string palavraSecreta = "TESOURO";
-string = resposta;
+string resposta;
 char[] progresso = new char[7];
 int tentativasRestantes = 10;
 Console.WriteLine("Decodificador");
@@ -12,26 +12,30 @@ for (int i = 0; i < 10; i++)
     switch (resposta)
     {
         case "t":
-            char[0] = "T";
-             break;
+            progresso[0] = 'T';
+            break;
         case "e":
-            char[1] = "E";
-             break;
+            progresso[1] = 'E';
+            break;
         case "s":
-            char[1] = "S";
-             break;
+            progresso[2] = 'S';
+            break;
         case "o":
-            char[1] = "O";
-             break;
+            progresso[3] = 'O';
+            progresso[6] = 'O';
+            break;
         case "u":
-            char[1] = "U";
-             break;
+            progresso[4] = 'U';
+            break;
         case "r":
-            char[1] = "R";
-             break;
-        case "o":
-            char[1] = "O";
-             break;
+            progresso[5] = 'R';
+            break;
+
     }
-    Console.WriteLine("Progressão:" + char[0] + "," + char[1] + "," + char{2] + "," + char[3] + "," + char[4] + "," + char[5] + "," + char[6] + ",");
+    Console.WriteLine("Progressão:" + progresso[0] + " | " + progresso[1] + " | " + progresso[2] + " | " + progresso[3] + " | " + progresso[4] + " | " + progresso[5] + " | " + progresso[6] + " . ");
+    if (progresso[0] == 'T' && progresso[1] == 'E' && progresso[2] == 'S' && progresso[3] == 'O' && progresso[4] == 'U' && progresso[5] == 'R' && progresso[6] == 'O')
+    {
+        Console.WriteLine("Parábens!Você acertou");
+        i = 10;
+    }
 }
